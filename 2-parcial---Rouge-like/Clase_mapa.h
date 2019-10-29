fd6c462265cbf826976780124694d8a627377929
JJEllacopulos <jj.ellacopulos@gmail.com>
2019-10-28 21:22:53 -0300

    28/10 21:22
    Mapa incompleto

--- 2-parcial---Rouge-like/Clase_mapa.h
+++ 2-parcial---Rouge-like/Clase_mapa.h
@@ -16,12 +16,17 @@
 #define MAX_FILAS_GENERAL 30
 #define MAX_COLUMNAS_GENERAL 30
 
-    ///Contenido de matriz:
+    ///Contenido de matriz de juego:
+#define MURO_INRROMPIBLE 88 ///X
 #define MURO 77 ///M
 #define COFRE 67 ///C
 #define AVATAR 65 ///A
 #define PUERTA 80 ///P
 #define PISO 112 ///p
+
+    ///Contenido de matriz guia:
+#define BLOQUE_ABILITADO 43 ///+
+#define BLOQUE_INABILITADO 45 ///-
 
     ///Biomas:
         ///Proximamente...
@@ -29,8 +34,8 @@
 
 class MAPA {
     private:
-        char mapa_guia[MAX_FILAS_GUIA][MAX_COLUMNAS_GUIA];
-        char mapa_juego[MAX_FILAS_GENERAL][MAX_COLUMNAS_GENERAL];
+        char mapa_guia[MAX_FILAS_GUIA][MAX_COLUMNAS_GUIA] = {BLOQUE_INABILITADO};
+        char mapa_juego[MAX_FILAS_GENERAL][MAX_COLUMNAS_GENERAL] = {MURO};
         int bioma;
     public:
         ///Constructor:
@@ -57,20 +62,124 @@ MAPA::MAPA(int bio){
 
 void MAPA::armar_mapa_guia(){
 
+    int x = 2;
+    int y = 2;
+
+    int mov;
+    int direccion;
+
+    bool key = true;
+
+    srand(time(0));
+
+    for(mov=0; mov<20; mov++){
+
+        while(key){
+
+            direccion = ((rand() ) % 4);
+
+            switch(direccion){
+                case 0: ///Arriba
+                    if((x - 1) > 0){
+                        if(mapa_guia[x-1][y] == BLOQUE_INABILITADO){
+                            key = false;
+                            mapa_guia[x-1][y] = BLOQUE_ABILITADO;
+                        }
+                        x--;
+                    }
+                break;
+                case 1: ///Abajo
+                    if((x + 1) < MAX_COLUMNAS_GUIA){
+                        if(mapa_guia[x+1][y] == BLOQUE_INABILITADO){
+                            key = false;
+                            mapa_guia[x+1][y] = BLOQUE_ABILITADO;
+                        }
+                        x++;
+                    }
+                break;
+                case 2: ///Izquierda
+                    if((y - 1) > 0){
+                        if(mapa_guia[y-1][y] == BLOQUE_INABILITADO){
+                            key = false;
+                            mapa_guia[y-1][y] = BLOQUE_ABILITADO;
+                        }
+                        y--;
+                    }
+                break;
+                case 3: ///Derecha
+                    if((y + 1) < MAX_FILAS_GUIA){
+                        if(mapa_guia[y+1][y] == BLOQUE_INABILITADO){
+                            key = false;
+                            mapa_guia[y+1][y] = BLOQUE_ABILITADO;
+                        }
+                        y++;
+                    }
+                break;
+            }
+
+        }
+
+        key = true;
+    }
+}
+
+
+
+void MAPA::armar_mapa_general(){
+
+    int x, y;
+
+    for(x=0;x<MAX_FILAS_GUIA;x++){
+        for(y=0;y<MAX_COLUMNAS_GUIA;y++){
+            if(mapa_guia[x][y] == BLOQUE_ABILITADO){
+                armar_mapa_general_marco();
+                armar_mapa_general_puertas();
+                armar_mapa_general_caminos();
+            }
+        }
+    }
+
+}
+
+
+
+void MAPA::armar_mapa_general_marco(){
+
 
 
 }
 
-void MAPA::armar_mapa_general();
+void MAPA::armar_mapa_general_puertas(){
+
+
+
+}
+
+void MAPA::armar_mapa_general_caminos(){
+
+
+
+}
 
 ///Gets:
-char MAPA::gets_mapa_guia();
-char MAPA::gets_mapa_general();
+char MAPA::gets_mapa_guia(){
+    return mapa_guia[posicion_en_x][posicion_en_y];
+}
+
+char MAPA::gets_mapa_general(){
+    return mapa_juego[posicion_x_mayor][posicion_y_mayor][posicion_x_menor][posicion_y_menor];
+}
+
 ///Sets:
 ///Ingresa un valor char a la pocicion de la matriz guia señalada.
-void MAPA::sets_mapa_guia(int posicion_en_x, int posicion_en_y, char auxiliar);
+void MAPA::sets_mapa_guia(int posicion_en_x, int posicion_en_y, char auxiliar){
+    mapa_guia[posicion_en_x][posicion_en_y] = auxiliar;
+}
+
 ///Ingresa un valor char a la pocicion de la matriz juego señalada.
-void MAPA::sets_mapa_general(int posicion_x_mayor, int posicion_y_mayor, int posicion_x_menor, int posicion_y_menor, char auxiliar);
+void MAPA::sets_mapa_general(int posicion_x_mayor, int posicion_y_mayor, int posicion_x_menor, int posicion_y_menor, char auxiliar){
+    mapa_juego[posicion_x_mayor][posicion_y_mayor][posicion_x_menor][posicion_y_menor] = auxiliar;
+}
 
 
 

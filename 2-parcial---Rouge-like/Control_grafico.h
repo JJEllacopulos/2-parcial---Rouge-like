ffca024e727a141622c4bea588c6cd85191a9d46
JJEllacopulos <jj.ellacopulos@gmail.com>
2019-11-03 15:21:59 -0300

    15:20 3/11
    Inicio y final de la libreria graficar.h
    Aun le faltan arreglar errores.
    
    Cargado los nuevas sprites.
    
    Ni yo me la creo al ver que hay 22 cambios de una.

--- 2-parcial---Rouge-like/Control_grafico.h
+++ 2-parcial---Rouge-like/Control_grafico.h
@@ -1,10 +1,27 @@
 #ifndef CONTROL_GRAFICO_H_INCLUDED
 #define CONTROL_GRAFICO_H_INCLUDED
 
+#define TAMAÑO_X_BUFFER 740 ///Tamaño del los buffer en el eje x.
+#define TAMAÑO_Y_BUFFER 740 ///Tamaño del los buffer en el eje y.
+
 #define TAMAÑO_X_SPRITE 30 ///Tamaño del los sprites en el eje x.
 #define TAMAÑO_Y_SPRITE 30 ///Tamaño del los sprites en el eje y.
 
-void leer_mapa(MAPA mapa_juego, int x_externo, int y_externo){
+
+void asignar_sprites(){
+
+    buffer = create_bitmap(TAMAÑO_X_BUFFER, TAMAÑO_Y_BUFFER);
+
+    MURO_INRROMPIBLE_spr = load_bitmap("Imagenes/Piedra_2.bmp", NULL);
+    MURO_ROMPIBLE_spr = load_bitmap("Imagenes/Piedra_3.bmp", NULL);
+    COFRE_spr = load_bitmap("Imagenes/Cofre_1.bmp", NULL);
+    AVATAR_spr = load_bitmap("Imagenes/Viajero_1.bmp", NULL);
+    PUERTA_spr = load_bitmap("Imagenes/Puerta_1.bmp", NULL);
+    PISO_spr = load_bitmap("Imagenes/Mosaico_2.bmp", NULL);
+
+}
+
+void Graficar_mapa_base(const MAPA &mapa, int x_externo = 0, int y_externo = 0){
 
     int x_interno;
     int y_interno;
@@ -12,7 +29,7 @@ void leer_mapa(MAPA mapa_juego, int x_externo, int y_externo){
     for(x_interno=0;x_interno<filas;x_interno++){
         for(y_interno=0;y_interno<columnas;y_interno++){
 
-            switch(mapa_juego.gets_mapa_juego(x_externo, y_externo, x_interno, y_interno)){
+            switch(mapa.gets_mapa_general(x_externo, y_externo, x_interno, y_interno)){
                 case MURO_INRROMPIBLE:
 
                     draw_sprite(buffer, MURO_INRROMPIBLE_spr, x_interno * TAMAÑO_X_SPRITE, y_interno * TAMAÑO_Y_SPRITE);
@@ -22,6 +39,28 @@ void leer_mapa(MAPA mapa_juego, int x_externo, int y_externo){
 
                     draw_sprite(buffer, MURO_ROMPIBLE_spr, x_interno * TAMAÑO_X_SPRITE, y_interno * TAMAÑO_Y_SPRITE);
                 break;
+
+                default:
+
+                    draw_sprite(buffer, PISO_spr, x_interno * TAMAÑO_X_SPRITE, y_interno * TAMAÑO_Y_SPRITE);
+                break;
+
+            }
+
+        }
+    }
+
+}
+
+void Graficar_mapa_objetos(const MAPA &mapa, int x_externo = 0, int y_externo = 0){
+
+    int x_interno;
+    int y_interno;
+
+    for(x_interno=0;x_interno<filas;x_interno++){
+        for(y_interno=0;y_interno<columnas;y_interno++){
+
+            switch(mapa_juego.gets_mapa_juego(x_externo, y_externo, x_interno, y_interno)){
 
                 case COFRE:
 
@@ -38,15 +77,16 @@ void leer_mapa(MAPA mapa_juego, int x_externo, int y_externo){
                     draw_sprite(buffer, PUERTA_spr, x_interno * TAMAÑO_X_SPRITE, y_interno * TAMAÑO_Y_SPRITE);
                 break;
 
-                case PISO:
-
-                    draw_sprite(buffer, PISO_spr, x_interno * TAMAÑO_X_SPRITE, y_interno * TAMAÑO_Y_SPRITE);
-                break;
-
             }
 
         }
     }
+
+}
+
+void pantallaso(){
+
+    blit(buffer, screen, 0, 0, 0, 0, TAMAÑO_X_BUFFER, TAMAÑO_Y_BUFFER);
 
 }
 
--- 2-parcial---Rouge-like/main.cpp
+++ 2-parcial---Rouge-like/main.cpp
@@ -8,6 +8,30 @@
 
 int main(){
 
+    allegro_init();
+    install_keyboard();
+
+    set_color_depth(32);
+    set_gfx_mode(GFX_AUTODETECT_WINDOWED, 740, 740, 0, 0);
+
+    asignar_sprites();
+
+    MAPA mapa();
+
+    while(!key[KEY_ESC]){
+
+
+        clear(buffer);
+
+        Graficar_mapa_base(mapa);
+        Graficar_mapa_objetos(mapa);
+
+        pantallaso();
+
+
+        rest(150);
+    }
+
 }
 
 END_OF_MAIN();
